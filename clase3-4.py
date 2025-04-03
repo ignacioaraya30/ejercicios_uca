1a60fd77717c1cd47251b8f0f6fae4a359ea4c54
ignacioaraya30 <nachoraraya27@gmail.com>
2025-04-03 18:35:41 -0300

    Add files via upload

---
+++ clase3-4.py
@@ -0,0 +1,10 @@
+#pedirle al usuario 2 valores, invertirlos, imprimirlos por pantalla
+
+valor3 =input("Ingrese un valor: ")
+valor4 = input("Ingrese un valor: ")
+
+valor5 = valor4
+valor6 = valor3
+
+print(valor5)
+print(valor6)
