b74897ab3371e07ce8f035670fca9aa8b338705f
ignacioaraya30 <nachoraraya27@gmail.com>
2025-04-10 19:52:32 -0300

    Add files via upload

---
+++ clase10-4.py
@@ -0,0 +1,14 @@
+
+#import random
+#numero = random.randint(1,100)
+#crear lista de 20 posiciones, agregarle a la lista numeros aleatorios entre 1 y 200
+
+#lista=[]
+#for i in range(20):
+#    numero = random.randint(1,100)
+#    if numero not in lista:
+#        lista.append(numero) 
+#print(lista)
+
+persona={"nombre":"Fernando", "edad":19, "ciudad": "Paraná"}
+print(persona["nombre"])
